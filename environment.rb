38a83161b42d162bf95319db6ff2cd4f8e3483f8
Kyle Drake <kyledrake@gmail.com>
2011-09-14 16:36:47 -0700

    Updates and consolidate configs

--- environment.rb
+++ environment.rb
@@ -8,22 +8,45 @@ Dir.glob(['lib', 'models'].map! {|d| File.join File.expand_path(File.dirname(__F
 puts "Starting in #{Sinatra::Base.environment} mode.."
 
 class Controller < Sinatra::Base
-  register Sinatra::Synchrony
-  register Sinatra::Namespace
-  register Sinatra::Flash
+
+  ##
+  # Application specific configuration
+  ##
+
+  raise 'Configuration error: session secret (and likely other settings) are not set. Change in environment.rb and remove the raise statement'
+  set :sessions,                 true
+  set :session_secret,           'PUT SOMETHING HERE'
+  set :geoloqi_memcache_enabled, false
+  set :geoloqi_memcache_url,     '127.0.0.1:11211'
+  set :geoloqi_client_id,        'PUT CLIENT ID HERE'
+  set :geoloqi_client_secret,    'PUT CLIENT SECRET HERE'
+
+
+  ##
+  # The rest of this you shouldn't need to change (initially).
+  ##
 
   set :raise_errors,    false
   set :show_exceptions, false
   set :method_override, true
   set :public,          'public'
-  set :sessions,        true
-  set :session_secret,  'PUT SOMETHING HERE'
   set :erubis,          :escape_html => true
+
+  register Sinatra::Synchrony
+  register Sinatra::Namespace
+  register Sinatra::Flash
+
+  Geoloqi.config :client_id => settings.geoloqi_client_id,
+                 :client_secret => settings.geoloqi_client_secret,
+                 :use_hashie_mash => true,
+                 :adapter => :em_synchrony
+
+  Geoloqi.config.logger = STDOUT if development?
 
   Faraday.default_adapter = :em_synchrony
 
   # async-aware Memcache client
-  # Cache = Dalli::Client.new '127.0.0.1:11211', :async => true
+  Cache = Dalli::Client.new(settings.geoloqi_memcache_url, :async => true) if settings.geoloqi_memcache_enabled?
 
   configure :development do
     use Rack::CommonLogger
